be2fe66024c01cd3403ce7510b0330d11a8c3594
asadbek0512 <khusanovasadbek777@gmail.com>
2025-06-22 02:17:23 +0900

    feat: develop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './app/MaterialTheme';
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import './css/index.css';
-import ContextProvider from './app/context/ContextProvider';
+import "./css/index.css";
+import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
 
 const container = document.getElementById("root")!;
-const root = createRoot(container)
+const root = createRoot(container);
 //GI: Redux, MUI, TAILWIND,  SocketIO, webSocket, ReactRouterDom, Context, Apoll ..
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>   {/*malumotlar bunkeri  hisoblanadi*/}
+  <Provider store={store}>
     <ContextProvider>
-      <ThemeProvider theme={theme}>{/* material ui */}
-        <CssBaseline />
-        <Router>
-          <App />
-        </Router>
-      </ThemeProvider>
+      <SocketProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <App />
+          </Router>
+        </ThemeProvider>
+      </SocketProvider>
     </ContextProvider>
   </Provider>
-  // </React.StrictMode>,
+  // </React.StrictMode >,
 );
 
 // If you want to start measuring performance in your app, pass a function
